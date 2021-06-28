82480a0d6f449610c319cc95d546df808f1748b3
Esteban Perugachi <estebanmh28@hotmail.com>
2021-06-28 04:26:01 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//crear un modulo de contador
+//declaraciones y export
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
+   
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,21 @@
+
+import { NgModule } from "@angular/core";
+
+import { ContatorComponent } from './contador/contador.compontent';
+
+
+@NgModule({
+declarations:[
+ContatorComponent
+
+],
+exports:[
+ContatorComponent
+
+]
+
+
+})
+export class ContadorModule {
+
+}
